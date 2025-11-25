b21672ed68c11f809065435cb7c6d83640fba34a
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-25 14:49:26 +0700

    menambahkan controller select/read/get

--- controllers/komikController.js
+++ controllers/komikController.js
@@ -17,3 +17,28 @@ async function createKomik(req, res) {
     res.status(400).json({ success: false, error: error.message });
   }
 }
+
+async function getAllKomik(req, res) {
+  try {
+    const result = await komikService.getAllKomik(db);
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+}
+
+async function getKomikById(req, res) {
+  try {
+    const { id } = req.params;
+    const result = await komikService.getKomikById(db, id);
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    res.status(404).json({ success: false, error: error.message });
+  }
+}
