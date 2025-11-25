03cead08b28b2571c281178aacd63dd28793f805
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-25 14:52:40 +0700

    menambahkan service delete

--- services/komikService.js
+++ services/komikService.js
@@ -48,3 +48,21 @@ async function updateKomik(database, id, komikData) {
   await komik.update(komikData);
   return komik;
 }
+
+async function deleteKomik(database, id) {
+  const komik = await database.Komik.findByPk(id);
+  if (!komik) {
+    throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+  }
+
+  await komik.destroy();
+  return { message: `Komik dengan ID ${id} berhasil dihapus` };
+}
+
+module.exports = {
+  createKomik,
+  getAllKomik,
+  getKomikById,
+  updateKomik,
+  deleteKomik,
+};
